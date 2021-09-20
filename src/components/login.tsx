de6781d3ab70bd12ed311ffe0f035c4b289653f5
Ivan Slavgorodsky <ivan.slavgorodsky@sharp-dev.net>
2021-09-20 09:46:01 +0700

    update login to formik and yup

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,46 +1,60 @@
-import React, { useState } from "react";
+import React from "react";
 import { useHistory } from "react-router-dom";
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import AppProps from './types';
 
 const Login = (props: AppProps) => {
   let history = useHistory();
-  const [formState, setFormState] = useState({email: "", password: ""});
-
-  const handleSubmit = (evt: React.FormEvent) => {
-    props.login(formState.email, formState.password)
-      .then( token => {
-        if (token) {
-          props.save("token", token);
-          props.remove("user");
-          props.remove("transactions");
-          history.push("/");
-        }
-      });
-    evt.preventDefault();
-  }
-
-  const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
-    setFormState({
-      ...formState,
-      [evt.target.name]: evt.target.value,
-    });
-  };
+  const formik = useFormik({
+    initialValues: {
+      password: '',
+      email: '',
+    },
+    validationSchema: Yup.object({
+      password: Yup.string()
+        .max(20, 'Must be 20 characters or less')
+        .required('Required'),
+      email: Yup.string()
+        .email('Invalid email address')
+        .required('Required'),
+    }),
+    onSubmit: values => {
+      props.login(formik.values.email, formik.values.password)
+        .then( token => {
+          if (token) {
+            props.save("token", token);
+            props.remove("user");
+            props.remove("transactions");
+            history.push("/");
+          }
+        });
+    },
+  });
 
   return (
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={formik.handleSubmit}>
           <h1>Sign In</h1>
           <div className="form-group m-3">
               <label>Email address</label>
-              <input name="email"
-                type="email" className="form-control" placeholder="Enter email"
-                value={formState.email} onChange={handleChange} required autoComplete="off" />
+              <input
+                name="email" type="email" className="form-control" placeholder="Enter email"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email} />
+                {formik.touched.email && formik.errors.email ?
+                  (<div className="alert-warning">{formik.errors.email}</div>) : null}
           </div>
 
           <div className="form-group m-3">
               <label>Password</label>
-              <input name="password"
-                type="password" className="form-control" placeholder="Enter password"
-                value={formState.password} onChange={handleChange} required autoComplete="off" />
+              <input
+                name="password" type="password" className="form-control" placeholder="Enter password"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.password} />
+                {formik.touched.password && formik.errors.password ?
+                  (<div className="alert-warning">{formik.errors.password}</div>) : null}
           </div>
 
           <div className="form-group m-3 d-flex justify-content-center align-items-center flex-column">
